a25e827e0f4eaebd0e954808d9aa9b8435787db2
u-kiyota <yu.kiyota@omeroid.com>
2021-12-16 15:30:59 +0900

    pagingコンポーネントのPrevボタンが使えないバグフィックス

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -72,23 +72,15 @@ const ListItem = (props) => {
     return(
         <section id="two" className="spotlights">
             { currentBlock.map(i => {
-                return <Item image={i.image} imageAlt={i.imageAlt} to={i.to} title={i.title} content={i.content} >{props.children}</Item>
+                return <Item key={i.title} image={i.image} imageAlt={i.imageAlt} to={i.to} title={i.title} content={i.content} >{props.children}</Item>
             }) }
             <Optional show={maxPage > 1}>
                 <div className="pagination-wrap">
                     <ul className="pagination">
-                        <li><span className={`button small ${currentPage == 1 ? 'disabled': ''}`}>Prev</span></li>
+                        <li><a href={`?page=${currentPage-1}`} className={`button small ${currentPage == 1 ? 'disabled': ''}`}>Prev</a></li>
                         { pageBlocks.map((_, index) => {
                             return <Page number={index+1} current={currentPage} />
                         }) }
-                        {/* <Page number={1} current={currentPage} /> */}
-                        {/* <li><a href="?page=1" className="page active">1</a></li>
-                        <li><a href="?page=2" className="page">2</a></li>
-                        <li><a href="?page=3" className="page">3</a>    </li>
-                        <li><span>&hellip;</span></li>
-                        <li><a href="#" className="page">8</a></li>
-                        <li><a href="#" className="page">9</a></li>
-                        <li><a href="#" className="page">10</a></li> */}
                         <li><a href={`?page=${currentPage+1}`} className={`button small ${currentPage == maxPage ? 'disabled': ''}`}>Next</a></li>
                     </ul>
                 </div>
