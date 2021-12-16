000dbe6f0c7c6928fc7d6179b6632a51678b733c
u-kiyota <yu.kiyota@omeroid.com>
2021-12-16 15:59:32 +0900

    Updates

--- sw.js
+++ sw.js
@@ -27,17 +27,17 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-b3be7e7b47436273bf09.js"
+    "url": "webpack-runtime-74fab42e83940de9122e.js"
   },
   {
     "url": "framework-caaca52af16d464f8089.js"
   },
   {
-    "url": "app-2c644b8cf68569ac0ac0.js"
+    "url": "app-4ecceaa371a1488b158d.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "9190d8efa085fc87082c1fcd6187c8bb"
+    "revision": "98d1b07e1079b2aefb496125c35bd10f"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
@@ -139,7 +139,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-2c644b8cf68569ac0ac0.js`))) {
+  if (!resources || !(await caches.match(`/app-4ecceaa371a1488b158d.js`))) {
     return await fetch(event.request)
   }
 
