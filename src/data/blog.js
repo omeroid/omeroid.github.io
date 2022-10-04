4dec0b4284599c1eb9b9d82d1a6ef2c68a3dd944
u-kiyota <yu.kiyota@omeroid.com>
2022-10-04 19:21:19 +0900

    ブログページにはTechBlogとNotionのリンクのみ記載

--- src/data/blog.js
+++ src/data/blog.js
@@ -1,39 +1,21 @@
 import React from 'react'
 
 import travel2020Image from '../assets/images/blog/travel2020.jpg'
-import logoImage from '../assets/images/logo/omeroid_icon.svg'
-import newBisinessImage from '../assets/images/blog/new-bisiness.png'
-import party2021Image from '../assets/images/blog/party2021.jpg'
+import techBlogImage from '../assets/images/news/omeroid-techblog.png'
 
 export default () => [
   {
-    "image": party2021Image,
-    "imageAlt": "創立3周年記念パーティー開催してみた！",
-    "to": "https://note.com/omeroid/n/nc70aa4880d8a",
-    "title": [<small>2021/12</small>, <br />, "[ブログ]創立3周年記念パーティー開催してみた！"],
-    "content": ["omeroid創立3周年と売上目標達成を記念してパーティーを行いました", <br />]
-  },
-  {
-    "image": newBisinessImage,
-    "imageAlt": "新規事業への取り組みを始めました",
-    "to": "https://note.com/omeroid/n/n0e0cb1249d0e",
-    "title": [<small>2020/11</small>, <br />, "[ブログ]新規事業への取り組み"],
-    "content": ["メンバーからアイデアを募り、仮説検証スプリントを回して、新規事業を創造するプログラムをはじました", <br />]
+    "image": techBlogImage,
+    "imageAlt": "TECH BLOG",
+    "to": "https://omeroid.notion.site/OMEROID-TECH-BLOG-b49535115c1b4316abbc4d579aa153f9",
+    "title": ["TECH BLOG"],
+    "content": ["omeroidの技術的な取り組み", <br />]
   },
   {
     "image": travel2020Image,
-    "imageAlt": "社員旅行2020",
-    "to": "https://note.com/omeroid/n/nf7d1e62fc1b2",
-    "title": [<small>2020/10</small>, <br />, "社員旅行2020"],
-    "content": ["メンバー8名で社員旅行に行ったきた様子をお届けします", <br />,
-    ]
-  },
-  {
-    "image": logoImage,
-    "imageAlt": "What is omeroid?",
-    "to": "https://note.com/omeroid/n/n874cf4ff76c6",
-    "title": ["What is omeroid?"],
-    "content": ["omeroidの社名の由来をご紹介します", <br />,
-    ]
+    "imageAlt": "COMPANY BLOG",
+    "to": "https://note.com/omeroid",
+    "title": ["COMPANY BLOG"],
+    "content": ["omeroidのイベントや文化", <br />]
   },
 ]
