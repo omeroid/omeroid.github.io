4e915274dc5a6f56d8db1610d6c40eb5543fd44d
u-kiyota <yu.kiyota@omeroid.com>
2023-07-19 18:08:27 +0900

    地図を修正

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -22,7 +22,7 @@ const Contact = (props) => (
                         <li><input type="reset" value="Clear" /></li>
                     </ul>
                 </form> */}
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d103830.75871456071!2d139.64967627861523!3d35.58551917108183!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x601861d2c978d4c7%3A0xbe3af69eb1ea2ee0!2z5rC45LqV44OT44Or!5e0!3m2!1sja!2sjp!4v1684801290015!5m2!1sja!2sjp" width="600" height="450" frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5882.464330296581!2d139.77295345690138!3d35.678013037910915!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188958b6c58567%3A0xcdd2199ea87a51a8!2z5rC45LqV44OT44Or77yI5YWr5LiB5aCA77yJ!5e0!3m2!1sja!2sjp!4v1689755874220!5m2!1sja!2sjp" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3298.5852516080654!2d135.16429647642937!3d34.2336069730892!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6000b293191322dd%3A0xc9d777405979bce!2z44Ov44Kr44Ok44Oe56ys77yS5Yao5aOr44Ob44OG44Or!5e0!3m2!1sja!2sjp!4v1684801503883!5m2!1sja!2sjp" width="600" height="450" frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
             </section>
             <section className="split">
@@ -59,13 +59,13 @@ const Contact = (props) => (
                         <h3>Address</h3>
                         <div><b>東京オフィス</b></div>
                         <span>東京都中央区八丁堀3-1-7<br />
-                        永井ビル2階
+                            永井ビル2階
                         </span>
                         <br />
                         <br />
                         <div><b>和歌山オフィス</b></div>
                         <span>和歌山県和歌山市湊紺屋町1-20<br />
-                        Kinowa（キノワ）201号
+                            Kinowa（キノワ）201号
                         </span>
                         <br />
                         <br />
