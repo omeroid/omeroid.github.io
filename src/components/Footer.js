fc43354d0fb00ea5cf8d3c4eeda51f765fd0a80e
u-kiyota <yu.kiyota@omeroid.com>
2024-06-25 09:20:11 +0900

    セキュリティポリシー情報、セキュリティ方針追加

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,7 +12,8 @@ const Footer = (props) => (
                 <li><a href="#" className="icon alt fa-linkedin"><span className="label">LinkedIn</span></a></li> */}
             </ul>
             <ul className="footer_menu">
-                <Link to="/privacyPolicy" className="link ">プライバシーポリシー</Link>
+                <Link to="https://omeroid.notion.site/e6608dcc19e3414aa7458e0a6bf00145?pvs=4" className="link ">プライバシーポリシー</Link>
+                <Link to="https://omeroid.notion.site/141be5a46ca842dab09bc66fbc965bd2?pvs=4" className="link">情報セキュリティ方針</Link>
             </ul>
             <ul className="copyright">
                 <li>&copy; 2020 omeroid Inc. All rights reserved.</li>
