96edc16e47b29bb89f9c246109f4a274dea3a774
u-kiyota <yu.kiyota@omeroid.com>
2024-11-25 17:19:48 +0900

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-a1cc3a9cf9d1fed1124f.js"
+    "url": "webpack-runtime-2a5b7bce8595a37a57a5.js"
   },
   {
     "url": "styles.8df6561c84602c2f2b40.css"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "dc1877f064308406a5d00a583ecdb901"
+    "revision": "a4a5b40db05e56b3e3f78749d6a32925"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-ad6431e4664bcf916d19.js"
